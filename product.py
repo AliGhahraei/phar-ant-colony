76c193f457bb45e8e821594de67da8e15c4352d4
Ali Ghahraei <aligf94@gmail.com>
2017-09-28 14:48:02 -0500

    Add passed_phases to Product and create main function

--- product.py
+++ product.py
@@ -5,6 +5,14 @@ class Product():
         self.cost = cost
         self.name = name
         self.date = date
+        self.passed_phases = set()
 
     def days_left(self):
         return datetime.now() - self.date
+
+
+if __name__ == '__main__':
+    main()
+
+
+def main():
