1e7708b8f8fb84356d75569d21360a08bcba92c8
kvinuk <kevinomuro@gmail.com>
2017-09-28 15:55:33 -0500

    Adds import csv method and sample data csv

--- product.py
+++ product.py
@@ -1,18 +1,30 @@
 from datetime import datetime
 from enum import Enum
+from csv import reader
 
 class Phase(Enum):
     TROQ = 1
 
 class Product():
-    def __init__(self, cost, name, date):
+    def __init__(self, cost, name, date, passed_phases=None):
         self.cost = cost
         self.name = name
         self.date = date
-        self.passed_phases = set()
+        self.passed_phases = passed_phases or set()
 
     def days_left(self):
         return datetime.now() - self.date
+
+    def import_csv(path):
+        products = []
+        with open(path, newline='') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=',')
+            for row in reader:
+                products.append(Product(cost = {key:value for (key, value) in zip(range(13), row["Costo"].split(","))}, 
+                                        name = row['Producto'], 
+                                        date = datetime.strptime(row['Fecha'], '%d/%m/%Y'),
+                                        passed_phases = {int(x) for x in row["Fases Pasadas"].split(",")}))
+        return products
 
 
 def main():
