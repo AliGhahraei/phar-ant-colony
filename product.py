23b6ce2471835bffffeb513c7a68ba8935d3d52b
Unknown <di_go21@hotmail.com>
2017-09-28 16:42:22 -0500

    Auto stash before merge of "master" and "origin/master"

--- product.py
+++ product.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from enum import Enum
 import csv
 
-dependencies = {1:set(),2:set(),3:set(),4:{1,2,3},5:{4},6:{4},7:{5,6},8:[],9:[8],10:[9],11:[],12:[],13:[11,12],14[13,10,7]}
+dependencies = {1:set(),2:set(),3:set(),4:{1,2,3},5:{4},6:{4},7:{5,6},8:[],9:[8],10:[9],11:[],12:[],13:[11,12],14:[13,10,7]}
 
 
 class Product():
@@ -25,10 +25,11 @@ class Product():
                                         date = datetime.strptime(row['Fecha'], '%d/%m/%Y'),
                                         passed_phases = {int(x) for x in row["Fases Pasadas"].split(",")}))
         return products
+
 #Calcula dependencia
 def depCalc(root,prod):
     costAcum = 0
-    if root in dependencies:
+    if len(dependencies[root]) != 0:
         for i in dependencies[root]:
             costAcum = costAcum + depCalc(i,prod)
         return costAcum
@@ -41,13 +42,13 @@ def cost(result):
     totalCost = 0
     for component in result:
         for product in result[component]:
-            totalCost + depCalc(component, product)
+           totalCost = totalCost + depCalc(component, product)
     return totalCost
 
 
 def main():
-    #result = {13:Product({1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10,11:11,12:12,13:13},"tractor",datetime.today()),10:Product(),7:Product()}
-    print("diego")
+    products = Product.import_csv("datos.csv")
+    print(cost({4:products}))
     #products = None
     #best = {'vector': random_permutation(products)}
 
