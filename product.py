095a0dd064176b8c9ee3273a71afac6210fe0791
Unknown <di_go21@hotmail.com>
2017-09-28 20:40:24 -0500

    add delayed products to the computation of the cost

--- product.py
+++ product.py
@@ -2,7 +2,8 @@ from datetime import datetime
 from enum import Enum
 import csv
 
-dependencies = {1:set(),2:set(),3:set(),4:{1,2,3},5:{4},6:{4},7:{5,6},8:[],9:[8],10:[9],11:[],12:[],13:[11,12],14:[13,10,7]}
+dependencies = {1: set(), 2: set(), 3: set(), 4: {1, 2, 3}, 5: {4}, 6: {4}, 7: {5, 6}, 8: [], 9: [8], 10: [9], 11: [],
+                12: [], 13: [11, 12], 14: [13, 10, 7]}
 
 
 class Product():
@@ -12,8 +13,9 @@ class Product():
         self.date = date
         self.passed_phases = passed_phases or set()
 
-    def days_left(self):
-        return self.date.hours - datetime.now().hours()
+    def hours_left(self):
+        hours = abs(self.date - datetime.now())
+        return hours.total_seconds() / 3600
 
     def import_csv(path):
         products = []
@@ -30,34 +32,42 @@ class Product():
                                        row["Fases Pasadas"].split(",")}))
         return products
 
-#Calcula dependencia
-def depCalc(root,prod):
+
+# Calcula dependencia
+def depCalc(root, prod):
     costAcum = 0
     if len(dependencies[root]) != 0:
-        costAcum += depCalc(max(dependencies[root]),prod)
+        costAcum += depCalc(max(dependencies[root]), prod)
         return costAcum
     else:
         return prod.cost[root]
-#Calcula paralelo
-#Calcula el costo de un resultado de combinancion usando el numero de pedidos retrasados y tiempo total
-#Que le toma realizar esa solucion
+
+
+# Calcula paralelo
+# Calcula el costo de un resultado de combinancion usando el numero de pedidos retrasados y tiempo total
+# Que le toma realizar esa solucion
 def cost(result):
     totalCost = 0
+    delayed = 0
     for component in result:
         for product in result[component]:
-           totalCost = totalCost + product.cost[component]+depCalc(component, product)
-           print(product.days_left())
-    return totalCost
+            if product.hours_left() - product.cost[component] + depCalc(component, product) < 0:
+                delayed += 1
+            totalCost = totalCost + product.cost[component] + depCalc(component, product)
+    # if(totalCost -
+    return delayed * totalCost
+
 
 def main():
     products = Product.import_csv("datos.csv")
-    print(cost({4:products}))
-    #products = None
-    #best = {'vector': random_permutation(products)}
+    print(cost({4: products}))
+    # products = None
+    # best = {'vector': random_permutation(products)}
 
 
 if __name__ == '__main__':
     main()
+
 
 def random_permutation(products):
     pass
